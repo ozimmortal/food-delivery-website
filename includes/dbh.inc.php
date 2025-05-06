f6ffbaf3c11fa3abc2a27c9f824b88c3ec43e2e7
oz <zaleambo@gmail.com>
2025-05-06 22:30:36 +0300

    removed smt

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,7 +4,7 @@ $host = "127.0.0.1";
 $port = "3306";
 $dbname = "dl";
 $dbusrn = "root";
-$dbpwd = "oliy2627";
+$dbpwd = "";
 
 // Correct DSN format
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
